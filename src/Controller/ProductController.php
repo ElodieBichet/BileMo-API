6f4d3095b1ed54df05946c22930983998416c319
Elodie Bichet <e_bichet@yahoo.fr>
2021-07-05 12:03:27 +0200

    Add details() method for products

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -6,9 +6,12 @@ use App\Entity\Product;
 use App\Repository\ProductRepository;
 use JMS\Serializer\SerializerInterface;
 use JMS\Serializer\SerializationContext;
+use JsonException;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Routing\Exception\ResourceNotFoundException;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class ProductController extends AbstractController
@@ -31,6 +34,22 @@ class ProductController extends AbstractController
     {
         $context = SerializationContext::create()->setGroups(array("product:list"));
         $data = $this->serializer->serialize($this->productRepository->findAll(), 'json', $context);
+
+        return new JsonResponse($data, JsonResponse::HTTP_OK, [], true);
+    }
+
+    /**
+     * @Route("/api/products/{id}", name="api_product_details", methods={"GET"})
+     */
+    public function details(Product $product = null): JsonResponse
+    {
+        if (!$product) {
+            $exception = new ResourceNotFoundException("Aucun produit trouvé avec cet identifiant");
+            return new JsonResponse($exception->getMessage(), JsonResponse::HTTP_NOT_FOUND);
+        }
+
+        $context = SerializationContext::create()->setGroups(array("product:details"));
+        $data = $this->serializer->serialize($product, 'json', $context);
 
         return new JsonResponse($data, JsonResponse::HTTP_OK, [], true);
     }
